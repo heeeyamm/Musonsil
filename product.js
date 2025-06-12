550081e43dc1d3bea8f3b504c6be8498e417049e
heeeyamm <dlgmldo12@naver.com>
2025-06-13 00:12:29 +0900

    Add files via upload

--- product.js
+++ product.js
@@ -42,7 +42,7 @@ function showToast() {
 // 기존 코드 그대로 두고 아래 함수만 맨 아래에 추가! (재고 품절시 품절뜨게)
 
 function checkStock(productName) {
-  fetch(`https://script.google.com/macros/s/AKfycbzGCLc2AATyGkcuEp_iVZVDxdyOaNG2gBt87JDNo6-jpC0mJq96f6IElKiaoelFR_6n/exec?name=${encodeURIComponent(productName)}`)
+  fetch("https://script.google.com/macros/s/AKfycby9M8xfIOgREdW2O5OEqbY5bpL85-hbiKXlYngmc9ggR-IscwFnvBR_MQ6ySM93c4aT/exec?name=Dancing%20Bossa%20Nova%201")
     .then(res => res.json())
     .then(data => {
       const stock = Number(data.stock);
